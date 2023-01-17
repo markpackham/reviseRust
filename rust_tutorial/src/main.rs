1fa0658b6c979b179062016862b026252d0e66ce
mark <markpackham1@gmail.com>
2023-01-17 13:21:58 +0000

    ask for user name
    https://youtu.be/ygL_xcavzQ4?t=742

--- rust_tutorial/src/main.rs
+++ rust_tutorial/src/main.rs
@@ -8,6 +8,11 @@ use std::cmp::Ordering;
 
 fn main() {
     println!("What is your name?");
+    // the "mut" stand for mutable so changeable
+    // variables in rust are immutable by default
+    let mut name = String::new();
+    let greeting: &str = "Nice to meet you";
+    io::stdin().read_line(buf: &mut name) Result<usize, Error>.expect(msg: "Didn't recieve input!");
     // if you see an ! at the end of a function it is a macro
-    println!("Hello, world!");
+    println!("Hello {}! {}", name.trim_end(), greeting);
 }
