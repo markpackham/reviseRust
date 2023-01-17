cf98a407fdcb69280d06b1d6b640100f9fc3e339
mark <markpackham1@gmail.com>
2023-01-17 13:30:07 +0000

    demo constants
    https://youtu.be/ygL_xcavzQ4?t=817

--- rust_tutorial/src/main.rs
+++ rust_tutorial/src/main.rs
@@ -7,15 +7,6 @@ use std::fs::File;
 use std::cmp::Ordering;
 
 fn main() {
-    println!("What is your name?");
-    // the "mut" stand for mutable so changeable
-    // variables in rust are immutable by default
-    let mut name = String::new();
-    let greeting: &str = "Nice to meet you";
-    io::stdin()
-    .read_line(&mut name)
-    .ok()
-    .expect("Failed to read line");
-    // if you see an ! at the end of a function it is a macro
-    println!("Hello {}! {}", name.trim_end(), greeting);
+    const ONE_MIL: u32 = 1_000_000;
+    const ONE_MIL2: u32 = 1000000;
 }
