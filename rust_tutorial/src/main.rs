2014624bfc70ac0c4004208853b54f86e378c5e6
mark <markpackham1@gmail.com>
2023-01-17 13:13:05 +0000

    create empty string to hold a name
    https://youtu.be/ygL_xcavzQ4?t=526

--- rust_tutorial/src/main.rs
+++ rust_tutorial/src/main.rs
@@ -1,4 +1,13 @@
 #![allow(unused)]
+
+use std::io;
+use rand::Rng;
+use std::io::{Write, BufReader, BufRead, ErrorKind};
+use std::fs::File;
+use std::cmp::Ordering;
+
 fn main() {
+    println!("What is your name?");
+    // if you see an ! at the end of a function it is a macro
     println!("Hello, world!");
 }
