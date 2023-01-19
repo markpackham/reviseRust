e3ddf3f646b870bc7ad3ea30daf838c2ae088d74
mark <markpackham1@gmail.com>
2023-01-19 14:17:44 +0000

    demo enum with function
    https://youtu.be/ygL_xcavzQ4?t=3353

--- rust_tutorial/src/main.rs
+++ rust_tutorial/src/main.rs
@@ -7,7 +7,39 @@ use std::fs::File;
 use std::cmp::Ordering;
 
 fn main() {
- let int_u8: u8 = 5;
- let int2_u8: u8 = 4;
- let int3_u32: u32 = (int_u8 as u32) + (int2_u8 as u32);
+     enum Day {
+      Monday,
+      Tuesday,
+      Wednesday,
+      Thursday,
+      Friday,
+      Saturday,
+      Sunday
+  }
+
+  // can define functions for the enum I want to work with
+  impl Day {
+      fn is_weekend(&self) -> bool {
+         // in Rust you will use match as much as you use If statements
+          match self {
+              Day::Saturday | Day::Sunday => true,
+              _ => false
+          }
+      }
+  }
+
+  let today:Day = Day::Monday;
+
+  match today {
+   Day::Monday => println!("Everyone hates Monday"),
+   Day::Tuesday => println!("Pancake day"),
+   Day::Wednesday => println!("Odin day"),
+   Day::Thursday => println!("Thor's day"),
+   Day::Friday => println!("Almost Weekend"),
+   Day::Saturday => println!("Weekend!!!"),
+   Day::Sunday => println!("Weekend!!!"),
+}
+
+println!("Is today the weekend {}", today.is_weekend());
+
 }
