98e218aad7bc1e096202c36cc1ac4aa26346b135
mark <markpackham1@gmail.com>
2023-01-19 14:37:26 +0000

    demo function calling
    https://youtu.be/ygL_xcavzQ4?t=3670

--- rust_tutorial/src/main.rs
+++ rust_tutorial/src/main.rs
@@ -6,29 +6,14 @@ use std::io::{Write, BufReader, BufRead, ErrorKind};
 use std::fs::File;
 use std::cmp::Ordering;
 
+
+fn say_hello(){
+   println!("Hello");
+}
+
 fn main() {
-
-   // vectors are growable arrays that can only store values of same type
-   let vec1: Vec<i32> = Vec::new();
-   let mut vec2 = vec![1,2,3,4];
-   vec2.push(5);
-   println!("1st: {}", vec2[0]);
-   let second: &i32 = &vec2[1];
-   match vec2.get(1){
-      Some(second) => println!("2nd: {}", second),
-      None => println!("No 2nd value"),
-   }
-
-   for i in &mut vec2 {
-      *i *=2;
-   }
-
-   for i in &vec2{
-      println!("{}", i);
-   }
-
-   println!("Vec length {}", vec2.len());
-   println!("Pop off last value: {:?}",vec2.pop());
+   say_hello();
+   
 
 
 
