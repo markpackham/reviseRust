8d09af8f8f1ae25d699a4afc8b223ed5ad6a9f85
mark <markpackham1@gmail.com>
2023-01-19 14:10:21 +0000

    demo casting via "as"
    https://youtu.be/ygL_xcavzQ4?t=3115

--- rust_tutorial/src/main.rs
+++ rust_tutorial/src/main.rs
@@ -7,24 +7,7 @@ use std::fs::File;
 use std::cmp::Ordering;
 
 fn main() {
- let st3 = String::from("x a b c d e f g g g g");
- let mut v1: Vec<char> = st3.chars().collect();
- v1.sort();
- v1.dedup();
- for char in v1{
-   println!("{}", char);
- }
- let st4: &str = "Random string";
- let mut st5: String = st4.to_string();
- println!("{}",st5);
- let byte_arr1 = st5.as_bytes();
- let st6 = &st5[0..6];
- println!("String length : {}", st6.len());
- st5.clear();
- let st6 = String::from("Just some");
- let st7 = String::from(" words");
- let st8 = st6 + &st7;
- for char in st8.bytes(){
-   println!("{}", char);
- }
+ let int_u8: u8 = 5;
+ let int2_u8: u8 = 4;
+ let int3_u32: u32 = (int_u8 as u32) + (int2_u8 as u32);
 }
