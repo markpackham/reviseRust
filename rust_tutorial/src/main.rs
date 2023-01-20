030bb58bd933e5c1b6c5db8842d8a806a11c52ec
mark <markpackham1@gmail.com>
2023-01-20 11:18:51 +0000

    show use of clone() operator
    https://youtu.be/ygL_xcavzQ4?t=4538

--- rust_tutorial/src/main.rs
+++ rust_tutorial/src/main.rs
@@ -5,17 +5,15 @@ use rand::Rng;
 use std::io::{Write, BufReader, BufRead, ErrorKind};
 use std::fs::File;
 use std::cmp::Ordering;
-
 use std::ops::Add;
 
-// Generic <T> just like C# T is a placeholder, you can use any letter
-// to add Generics we need to use std::ops::Add;
-fn get_sum_gen<T:Add<Output = T>>(x: T, y: T)->T{
-   return x + y;
-}
 
 fn main() {
-   println!("5 + 4 = {}", get_sum_gen(5,4));
-   println!("50000000 + 40000000 = {}", get_sum_gen(500000000,400000000));
-   println!("5.1 + 4.1 = {}", get_sum_gen(5.1,4.1));
+   let str1 = String::from("World");
+   // clone() duplicates str1's value
+   // if I used the assignment operator rather than clone()
+   // then str1 would lose it's value in memory
+   let str2: String = str1.clone();
+   // str1 would be empty without clone since str2 would steal it's value
+   println!("Hello {}",str1);
 }
