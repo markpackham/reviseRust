6c19d4a3f62df00225866bc4e7aa5f27cabdc07a
mark <markpackham1@gmail.com>
2023-01-24 14:53:29 +0000

    spawn a thread
    https://youtu.be/ygL_xcavzQ4?t=8308

--- rust_tutorial/src/main.rs
+++ rust_tutorial/src/main.rs
@@ -11,6 +11,12 @@ use std::thread;
 use std::time::Duration;
 
 fn main() {
-
+   
+thread::spawn(move || {
+   for i in 1..10{
+      println!("Spawned thread: {}", i);
+      thread::sleep(Duration::from_millis(1));
+   }
+});
 
 }
