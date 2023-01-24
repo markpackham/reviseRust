dc35f41b05e9f5214024ab3aedd7a3c39db2faf5
mark <markpackham1@gmail.com>
2023-01-24 14:50:00 +0000

    use the thread and time duration libraries
    https://youtu.be/ygL_xcavzQ4?t=8233

--- rust_tutorial/src/main.rs
+++ rust_tutorial/src/main.rs
@@ -7,35 +7,10 @@ use std::fs::File;
 use std::cmp::Ordering;
 use std::ops::Add;
 use std::collections::HashMap;
+use std::thread;
+use std::time::Duration;
 
 fn main() {
 
-    struct TreeNode<T> {
-      pub left: Option<Box<TreeNode<T>>>,
-      pub right: Option<Box<TreeNode<T>>>,
-      pub key: T,
-    }
-
-    impl<T> TreeNode<T> {
-      pub fn new(key: T) -> Self{
-         TreeNode {left: None, right: None, key,
-         }
-      }
-
-      pub fn left(mut self, node: TreeNode<T>) -> Self{
-         self.left = Some(Box::new(node));
-         self
-       }
-   
-       pub fn right(mut self, node: TreeNode<T>) -> Self{
-         self.right = Some(Box::new(node));
-         self
-       }
-    }
-
-    // create root node and assign left and right branches
-    let node1 = TreeNode::new(1)
-    .left(TreeNode::new(2))
-    .right(TreeNode::new(3));
 
 }
