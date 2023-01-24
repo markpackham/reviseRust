4bb212e7d701de67ddafe20395364c55cd1fa519
mark <markpackham1@gmail.com>
2023-01-24 15:28:48 +0000

    show bank withdrawal using thread
    https://youtu.be/ygL_xcavzQ4?t=8753

--- rust_tutorial/src/main.rs
+++ rust_tutorial/src/main.rs
@@ -18,4 +18,20 @@ fn main() {
    fn withdraw(the_bank: &mut Bank, amt:f32){
       the_bank.balance -= amt;
    }
+
+   let mut bank = Bank{balance: 100.0};
+   withdraw(&mut bank, 5.00);
+   println!("Balance : {}", bank.balance);
+
+   // demo cocurrency via customers withdrawing money
+   // use threads to show money being drawn in more realistic times
+   fn customer(the_bank: &mut Bank){
+      withdraw(the_bank, 5.00);
+   }
+
+   thread::spawn(||{
+      customer(&mut bank)
+   }).join().unwrap();
+
+   // In Rust a closure must not outlive the current function
 }
