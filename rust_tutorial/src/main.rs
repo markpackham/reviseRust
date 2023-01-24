34671f5fe543395cb556a7f9a736d8f6ed0dadea
mark <markpackham1@gmail.com>
2023-01-24 15:35:38 +0000

    create bank withdrawal process
    https://youtu.be/ygL_xcavzQ4?t=9004

--- rust_tutorial/src/main.rs
+++ rust_tutorial/src/main.rs
@@ -9,29 +9,27 @@ use std::ops::Add;
 use std::collections::HashMap;
 use std::thread;
 use std::time::Duration;
+use std::rc::Rc;
+use std::cell::RefCell;
+// Arc provides shared ownership of same value
+// block locked threads via Mutex
+use std::sync::{Arc, Mutex};
 
 fn main() {
    
    pub struct Bank{
       balance: f32
    }
-   fn withdraw(the_bank: &mut Bank, amt:f32){
-      the_bank.balance -= amt;
+
+   fn withdraw(the_bank: &Arc<Mutex<Bank>>, amount:f32){
+      let mut bank_ref = the_bank.lock().unwrap();
+      if bank_ref.balance < 5.00{
+         println!("Current Balance: {} Widthdraw a smaller amount", bank_ref.balance);
+      } else{
+         bank_ref.balance -= amount;
+         println!("Customer withdrew {}, Current balance {}", amount, bank_ref.balance);
+      }
+
    }
-
-   let mut bank = Bank{balance: 100.0};
-   withdraw(&mut bank, 5.00);
-   println!("Balance : {}", bank.balance);
-
-   // demo cocurrency via customers withdrawing money
-   // use threads to show money being drawn in more realistic times
-   fn customer(the_bank: &mut Bank){
-      withdraw(the_bank, 5.00);
-   }
-
-   thread::spawn(||{
-      customer(&mut bank)
-   }).join().unwrap();
-
-   // In Rust a closure must not outlive the current function
+   
 }
